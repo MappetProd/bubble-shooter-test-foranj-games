4fab0e993e9b4e5a2c7e0ecaf71dc5bbe36f3870
MappetProd <fwexample821@gmail.com>
2024-09-30 18:36:38 +0300

    core movement added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    [SerializeField]
+    private float maxPowerDistance = 1f;
+
+    [SerializeField]
+    private float maxBallSpeed = 5f;
+    private float shotSpeed = 0f;
+
+    private Vector3 startHoldMousePos = Vector3.zero;
+    private Vector2 shotDirection = Vector2.zero;
+    private float shotPower = 0f;
+
+    private bool isHolding = false;
+    private bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +27,58 @@ public class BallController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (isMoving)
+        {
+            Vector3 velocity = shotDirection * shotSpeed;
+            Vector3 displacement = velocity * Time.deltaTime;
+
+            /*if (!World.instance.gameField.Contains(transform.position + displacement))
+            {
+                shotDirection = Vector2.Reflect();
+                Debug.Log(shotDirection);
+                return;
+            }*/
+
+            transform.position += displacement;
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !isHolding)
+        {
+            startHoldMousePos = Input.mousePosition;
+            startHoldMousePos.z = Camera.main.nearClipPlane;
+            startHoldMousePos = Camera.main.ScreenToWorldPoint(startHoldMousePos);
+
+            Debug.Log(startHoldMousePos);
+            isHolding = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 currMousePos = Input.mousePosition;
+            currMousePos.z = Camera.main.nearClipPlane;
+            currMousePos = Camera.main.ScreenToWorldPoint(currMousePos);
+            Debug.Log(currMousePos);
+
+            shotDirection = -(currMousePos - startHoldMousePos);
+            Debug.Log(shotDirection);
+
+            float holdDistance = (currMousePos - startHoldMousePos).magnitude;
+            shotPower = holdDistance < maxPowerDistance ? holdDistance : maxPowerDistance;
+            shotSpeed = maxBallSpeed * shotPower;
+
+            isHolding = false;
+            isMoving = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        shotDirection = Vector2.Reflect(shotDirection, collision.contacts[0].normal);
+    }
+
+    private void OnMouseOver()
     {
         
     }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class GameCollider : MonoBehaviour
+public class World : MonoBehaviour
 {
-    Camera camera;
+    [HideInInspector]
+    public static World instance;
+
+    [HideInInspector]
+    public Rect gameField;
+
+    private Camera camera;
     void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
         camera = Camera.main;
-        
         Vector2[] boundaries = GetWorldBoundaries();
         CreateCollider(boundaries);
+        gameField = GetWorldRect();
     }
 
     private Vector2[] GetWorldBoundaries()
@@ -27,7 +38,23 @@ public class GameCollider : MonoBehaviour
         Vector3 topRight = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.nearClipPlane));
 
         Vector2[] result = new Vector2[]{bottomLeft, topLeft, topRight, bottomRight, bottomLeft};
+        // return new Rect(bottomLeft.x, bottomLeft.y, (bottomRight - bottomLeft).magnitude, (topLeft - bottomLeft).magnitude);
         return result;
+    }
+
+    private Rect GetWorldRect()
+    {
+        if (camera == null || !camera.orthographic)
+        {
+            return new Rect();
+        }
+
+        Vector3 bottomLeft = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        Vector3 topLeft = camera.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, camera.nearClipPlane));
+        Vector3 bottomRight = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, camera.nearClipPlane));
+        Vector3 topRight = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth, camera.pixelHeight, camera.nearClipPlane));
+
+        return new Rect(bottomLeft.x, bottomLeft.y, (bottomRight - bottomLeft).magnitude, (topLeft - bottomLeft).magnitude);
     }
 
     private void CreateCollider(Vector2[] boundaries)
